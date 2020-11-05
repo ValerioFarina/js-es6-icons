e2859cd37a252114d0709b34bcb5fa26e542b418
Valerio Farina <valerio.farina.1994@gmail.com>
2020-11-05 14:03:21 +0100

    Milestone 1: creato array di icone

---
+++ main.js
@@ -0,0 +1,109 @@
+// Milestone 1: definire un array di oggetti; ogni oggetto rappresenta un'icona, che è caratterizzata da: nome, prefisso, tipo e famiglia.
+
+$(document).ready(function() {
+
+    // we create an array of objects
+    // each object represents an icon
+    // each icon has a name, a prefix, a type and a family
+    const icons = [
+        {
+            name: 'cat',
+            prefix: 'fa-',
+            type: 'animal',
+            family: 'fas'
+        },
+        {
+            name: 'crow',
+            prefix: 'fa-',
+            type: 'animal',
+            family: 'fas'
+        },
+        {
+            name: 'dog',
+            prefix: 'fa-',
+            type: 'animal',
+            family: 'fas'
+        },
+        {
+            name: 'dove',
+            prefix: 'fa-',
+            type: 'animal',
+            family: 'fas'
+        },
+        {
+            name: 'dragon',
+            prefix: 'fa-',
+            type: 'animal',
+            family: 'fas'
+        },
+        {
+            name: 'horse',
+            prefix: 'fa-',
+            type: 'animal',
+            family: 'fas'
+        },
+        {
+            name: 'hippo',
+            prefix: 'fa-',
+            type: 'animal',
+            family: 'fas'
+        },
+        {
+            name: 'fish',
+            prefix: 'fa-',
+            type: 'animal',
+            family: 'fas'
+        },
+        {
+            name: 'carrot',
+            prefix: 'fa-',
+            type: 'vegetable',
+            family: 'fas'
+        },
+        {
+            name: 'apple-alt',
+            prefix: 'fa-',
+            type: 'vegetable',
+            family: 'fas'
+        },
+        {
+            name: 'lemon',
+            prefix: 'fa-',
+            type: 'vegetable',
+            family: 'fas'
+        },
+        {
+            name: 'pepper-hot',
+            prefix: 'fa-',
+            type: 'vegetable',
+            family: 'fas'
+        },
+        {
+            name: 'user-astronaut',
+            prefix: 'fa-',
+            type: 'user',
+            family: 'fas'
+        },
+        {
+            name: 'user-graduate',
+            prefix: 'fa-',
+            type: 'user',
+            family: 'fas'
+        },
+        {
+            name: 'user-ninja',
+            prefix: 'fa-',
+            type: 'user',
+            family: 'fas'
+        },
+        {
+            name: 'user-secret',
+            prefix: 'fa-',
+            type: 'user',
+            family: 'fas'
+        }
+    ];
+    console.log(icons);
+
+
+});
